2d0153c64364995e3a161b7631249b527fa28223
kasimkid <kasim85.ra@gmail.com>
2024-01-22 23:08:32 -0300

    create form box and style input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { MyRoutes } from "./router/routes";
+export const ThemeContext = React.createContext(null)
+
 
 function App() {
+  const [theme, setTheme]=useState('ligth')
+
+
+
   return (
     <>
       <MyRoutes/>
