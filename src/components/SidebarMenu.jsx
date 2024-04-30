ccca79f036cb181c04726eff0bfc4c02c1b37216
kasimkid <kasim85.ra@gmail.com>
2024-04-29 23:53:50 -0400

    create Menu Links

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import logo from '../assets/react.svg';
 import { v } from '../styles/Variables';
-import { AiOutlineLeft } from 'react-icons/ai';
+import { AiOutlineLeft, AiOutlineHome } from 'react-icons/ai';
+import { MdOutlineAnalytics } from 'react-icons/md';
+import {Link} from 'react-router-dom';
 
 export function SidebarMenu({ sidebarOpen, setSidebarOpen }) {
 
@@ -20,25 +22,65 @@ export function SidebarMenu({ sidebarOpen, setSidebarOpen }) {
           <img src={logo} />
         </div>
         <div>
-          <h2>Usuario</h2>
+          <h2>Rafael</h2>
         </div>
       </div>
+    { linksArray.map(({icon, label, to}) => (
+      <div className='LinkContainer' key= 'label'>
+        <Link to={ to }>
+          <div className='Linkicon'>
+            {icon}
+          </div>
+          {sidebarOpen &&(
+            <span>{label}</span>
+          )
 
+          }
+
+        </Link>
+
+      </div>
+
+    ))}
     </Container>
   )
 }
+const linksArray = [
+{
+  label: 'Home',
+  icon: <AiOutlineHome/>,
+  to:'/'
+},
+{
+  label: 'Nuevo Ingreso',
+  icon: <MdOutlineAnalytics/>,
+  to:'/nuevo_ingreso'
+
+},
+{
+  label: 'Vehículos',
+  icon: <AiOutlineHome/>,
+  to:'/vehiculos'
+
+},
+]
+
+
+
+
+//Styled COmponents
 const Container = styled.div`
 color: ${(props) => props.theme.text};
 background: ${(props) => props.theme.bg};
 position:sticky;
-padding-top:20px;
+padding-top:10px;
 
 .Sidebarbutton{
   position: absolute;
   top: ${v.xxlSpacing};
   right: -18px;
-  width: 32px;
-  height: 32px;
+  width: 22px;
+  height: 22px;
   border-radius: 50%;
   background:${(props) => props.theme.bgtgderecha};
   box-shadow: 0 0 4px ${(props) => props.theme.bg3}, 0 0 7px ${(props) => props.theme.bg};
@@ -47,16 +89,24 @@ padding-top:20px;
   justify-content: center;
   cursor: pointer;
   transition: all 0.3s;
-  transform:${({isOpen}) => (isOpen ?`initial` : `rotate(180deg)`)};
+  transform:${({isOpen}) => (isOpen ? `initial` : `rotate(180deg)`)};
+  border: none;
+  letter-spacing: inherit;
+  color: inherit;
+  font-size: inherit;
+  padding:0;
+  font-family: inherit;
+  outline:none;
 }
-.logocontent{
+.Logocontent{
   display: flex;
   justify-content: center;
   align-items: center;
   padding-bottom:${v.lgSpacing};
+
   .imgcontent{
     display: flex;
-    img{
+    img {
       max-width:100%;
       height:auto;
     }
@@ -66,10 +116,24 @@ padding-top:20px;
     `scale(0.7)` : `scale(1.5)`)};
   }
   h2{
-    display:${({ isOpen }) => (!isOpen ?
+    
+    display:${({ isOpen }) => (isOpen ?
     `block` : `none`)};
+    
+  }
+  .LinkContainer {
+    margin: 9px 0;
+    padding: 0 15%;
+    :hover{
+      background:${(props) => props.theme.bg3};
+
+
+    }
+
   }
 }
 `
+
+
 export default SidebarMenu
 
